1ea455b52fbf4f873c22f9235535b26b26c033f3
Felipe <f.92xavier1@gmail.com>
2024-04-03 17:59:49 -0300

    template atualizado

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,14 +1,27 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+body {
+    background: ${(props) => props.theme.colors.background};
+    -webkit-font-smoothing: antialiased !important;
+    padding: 1rem;
+  }
+
   html, body, #root {
     min-height: 100%;
   }
-  body {
-    background: ${(props) => props.theme.colors.gray900};
-    color: ${(props) => props.theme.colors.gray600};
-    font-family: ${(props) => props.theme.fontFamily.sans};
-    padding: 1rem;
+
+  body, input, p, a, button {
+    color: ${(props) => props.theme.colors.text};
+    font-size: ${(props) => props.theme.fontSizes.normal};
+    font-family: ${(props) => props.theme.fonts};
   }
 
   h1, h2, h3, h4, h5, h6 {
